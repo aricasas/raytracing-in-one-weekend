bc7950a421642c2a1f8fea7c1cbea156437fece1
Ari <aricasasn@gmail.com>
2020-12-12 16:04:51 -0600

    First ppm image!

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,21 @@ fn main() {
 
     // Render
     print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        eprint!("\rScanlines remaining: {}", j);
+        for i in 0..IMAGE_WIDTH {
+            let r: f64 = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let g: f64 = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let b: f64 = 0.25;
+
+            let r: u32 = (255.999 * r) as u32;
+            let g: u32 = (255.999 * g) as u32;
+            let b: u32 = (255.999 * b) as u32;
+
+            println!("{} {} {}", r, g, b);
+        }
+    }
+
+    eprintln!("\nDone.")
 }
