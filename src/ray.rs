83a97ae09bef72e897a95ed4dedbb3841e2a5b9a
Ari <aricasasn@gmail.com>
2021-01-25 15:16:40 -0600

    Simplified sphere intersection formula

--- src/ray.rs
+++ src/ray.rs
@@ -32,16 +32,16 @@ impl Ray {
 
     fn hit_sphere(&self, center: Vec3, radius: f64) -> f64 {
         let oc = self.origin - center;
-        let a = Vec3::dot(&self.direction, &self.direction);
-        let b = 2.0 * Vec3::dot(&oc, &self.direction);
-        let c = Vec3::dot(&oc, &oc) - radius.powi(2);
+        let a = self.direction.length_squared();
+        let half_b = Vec3::dot(&oc, &self.direction);
+        let c = oc.length_squared() - radius.powi(2);
 
-        let discriminant = b.powi(2) - 4.0 * a * c;
+        let discriminant = half_b.powi(2) - a * c;
 
         if discriminant < 0.0 {
             -1.0
         } else {
-            (-b - discriminant.sqrt()) / (2.0 * a)
+            (-half_b - discriminant.sqrt()) / (a)
         }
     }
 }
