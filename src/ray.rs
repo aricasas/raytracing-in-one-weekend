9a6d85a78f940202c87c8a5310f90896fa955919
Ari <aricasasn@gmail.com>
2021-01-25 14:48:39 -0600

    First rendered sphere!

--- src/ray.rs
+++ src/ray.rs
@@ -18,9 +18,24 @@ impl Ray {
     }
 
     pub fn color(&self) -> Color {
+        if self.hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5) {
+            return Color::new(1.0, 0.0, 0.0);
+        }
+
         let unit_direction = self.direction.unit_vector();
         let t = 0.5 * (unit_direction.y() + 1.0);
         Color::linear_blend(t, &Color::new(1.0, 1.0, 1.0), &Color::new(0.5, 0.7, 1.0))
+    }
+
+    fn hit_sphere(&self, center: Vec3, radius: f64) -> bool {
+        let oc = self.origin - center;
+        let a = Vec3::dot(&self.direction, &self.direction);
+        let b = 2.0 * Vec3::dot(&oc, &self.direction);
+        let c = Vec3::dot(&oc, &oc) - radius.powi(2);
+
+        let discriminant = b.powi(2) - 4.0 * a * c;
+
+        discriminant > 0.0
     }
 }
 
