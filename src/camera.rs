d8cc7467905e22e1d832ec5ef322875ea39071c5
Ari <aricasasn@gmail.com>
2021-01-26 16:37:59 -0600

    Camera class

---
+++ src/camera.rs
@@ -0,0 +1,38 @@
+use super::ray::Ray;
+use super::vec3::Vec3;
+
+pub struct Camera {
+    origin: Vec3,
+    lower_left_corner: Vec3,
+    horizontal: Vec3,
+    vertical: Vec3,
+}
+
+impl Camera {
+    pub fn new() -> Self {
+        let aspect_ratio = 16.0 / 9.0;
+        let viewport_height = 2.0;
+        let viewport_width = viewport_height * aspect_ratio;
+        let focal_length = 1.0;
+
+        let origin = Vec3::new(0.0, 0.0, 0.0);
+        let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+        let vertical = Vec3::new(0.0, viewport_height, 0.0);
+        let lower_left_corner =
+            origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+
+        Self {
+            origin,
+            horizontal,
+            vertical,
+            lower_left_corner,
+        }
+    }
+
+    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
+        Ray::new(
+            self.origin,
+            self.lower_left_corner + (self.horizontal * u) + (self.vertical * v) - self.origin,
+        )
+    }
+}
